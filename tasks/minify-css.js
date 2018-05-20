e991d0b81d2324360635ed17a86e47c6390519c2
Ion Drimba F <iondrimba@users.noreply.github.com>
2018-05-20 15:40:24 -0300

    audio policy fix (#1)

--- tasks/minify-css.js
+++ tasks/minify-css.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 
 module.exports = function () {
-    return gulp.src('./public/css/*.css')
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('./public/css'));
+  return gulp.src('./public/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./public/css'));
 };
--- tasks/post-css.js
+++ tasks/post-css.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
 module.exports = function () {
-    return gulp.src('./public/css/demo.css')
-        .pipe(postcss([require('autoprefixer')]))
-        .pipe(gulp.dest('./public/css/'));
+  return gulp.src('./public/css/demo.css')
+    .pipe(postcss([require('autoprefixer')]))
+    .pipe(gulp.dest('./public/css/'));
 };
